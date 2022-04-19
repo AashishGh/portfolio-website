d05ca04f0ec43e6d8f366db60d019a8a6058ff90
Aashish Ghimire <39885345+AashishGh@users.noreply.github.com>
2022-04-19 18:13:48 +0545

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Data Analytics", "Developer", "ML Engineer",  "Freelancer"],
+        strings: ["Data Analyst", "Web Developer", "ML Engineer",],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Data Analytics", "Developer", "ML Engineer",  "Freelancer"],
+        strings: ["Data Analyst", "Web Developer", "ML Engineer",  ],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
